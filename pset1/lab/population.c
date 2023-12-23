68316583c183f8b24b3e3c6d43ddb36fdc57d3b6
ikmrn <ibragimovkmrn@gmail.com>
2023-12-23 22:24:06 +0500

    feat: Calculate number of years to reach end

--- pset1/lab/population.c
+++ pset1/lab/population.c
@@ -6,17 +6,21 @@ int main(void) {
     // TODO: Prompt for start size
     int start_size;
     do {
-        start_size =
-            get_int("Enter starting population size(9 is the minimum): ");
+        start_size = get_int("Start size: ");
     } while (start_size < 9);
 
     // TODO: Prompt for end size
     int end_size;
     do {
-      end_size = get_int("Enter ending population size: ");
-    }
-    while (end_size < start_size)
+        end_size = get_int("End size: ");
+    } while (end_size < start_size);
     // TODO: Calculate number of years until we reach threshold
-    
+    int n_years = 0;
+    int cur_pop = start_size;
+    while (cur_pop < end_size) {
+        cur_pop = cur_pop + (int)(cur_pop / 3) - (int)(cur_pop / 4);
+        n_years++;
+    }
     // TODO: Print number of years
+    printf("Years: %i\n", n_years);
 }
