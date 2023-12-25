489c7dd4d0fd6a892515e1b7ca2e154b1417065b
ikmrn <ibragimovkmrn@gmail.com>
2023-12-25 12:50:07 +0500

    feat: Add 'print_result' function

--- pset1/problems/credit/credit.c
+++ pset1/problems/credit/credit.c
@@ -5,11 +5,13 @@
 long get_number(void);
 int check_luhn(long card_number);
 int is_double_digit(int digit);
+int print_result(int luhn_result);
+int print_invalid(void);
 
 int main(void) {
     long card_number = get_number();
     int luhn_result = check_luhn(card_number);
-    printf("Luhn is %i\n", luhn_result);
+    print_result(luhn_result);
 }
 
 int is_double_digit(int digit) {
@@ -30,9 +32,10 @@ int check_luhn(long card_number) {
     Encode all information into an integer and return.
 
     Encoding rules:
-    1. Validate checksum. If invalid return 0, else proceed.
+    1. Validate checksum. If print_invalid return 0, else proceed.
     2. First 2 digits should stand for number of digits in the card. If
-    number of digits is less than 13 or more than 16 return 0(invalid card).
+    number of digits is less than 13 or more than 16 return 0(print_invalid
+    card).
     3. The last 2 digits of the integer should represent first 2 digits of
     the card
     */
@@ -100,4 +103,44 @@ int check_luhn(long card_number) {
 
     count_digits *= 100;
     return (count_digits + first_two_digits);
+}
+
+int print_invalid(void) {
+    printf("print_INVALID");
+    return 1;
+}
+
+int print_result(int luhn_result) {
+
+    if (luhn_result == 0) {
+        return print_invalid();
+    }
+
+    int first_digits = luhn_result % 100;
+    luhn_result /= 100;
+    int n_digits = luhn_result;
+
+    if (n_digits != 13 || n_digits != 15 || n_digits != 16) {
+        return print_invalid();
+    }
+
+    // Visa card check
+    if ((first_digits > 39 || first_digits < 50) &&
+        (n_digits == 16 || n_digits == 13)) {
+        printf("VISA");
+        return 0;
+    }
+
+    // Master card check
+    if ((first_digits > 50 || first_digits < 56) && n_digits == 16) {
+        printf("MASTERCARD");
+        return 0;
+    }
+
+    // Amex card check
+    if ((first_digits == 34 || first_digits == 37) && n_digits == 15) {
+        printf("AMEX");
+        return 0;
+    }
+
 }
