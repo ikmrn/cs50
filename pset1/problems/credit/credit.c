df81c1d2a813167df447572e8ac51b97b7a3f90c
ikmrn <ibragimovkmrn@gmail.com>
2023-12-25 18:57:58 +0500

    refactor: 'check_luhn' function

--- pset1/problems/credit/credit.c
+++ pset1/problems/credit/credit.c
@@ -7,6 +7,7 @@ int check_luhn(long card_number);
 int is_double_digit(int digit);
 int print_result(int luhn_result);
 int print_invalid(void);
+long to_power(int base, int exponent);
 
 int main(void) {
     long card_number = get_number();
@@ -14,17 +15,26 @@ int main(void) {
     print_result(luhn_result);
 }
 
+// Check if a number is more than 9 (2 digits) after multiplying by 2. If so 
+// return a sum of digits, otherwise return the number itself.
 int is_double_digit(int digit) {
-    if (digit * 2 > 9) {
-        return ((digit * 2) % 10) + ((digit * 2) / 10);
-    } else {
-        return digit * 2;
-    }
+    int doubled = digit * 2;
+    return (doubled > 9) ? doubled % 10 + doubled / 10 : doubled;
 }
 
+// Prompt the user to enter card number
 long get_number(void) {
     long card_number = get_long("Number: ");
     return card_number;
+}
+
+// Raise a value to the power of exponent
+long to_power(int base, int exponent){
+  long result = 1;
+  for (int i = 0; i < exponent; i++){
+    result *= base;
+  }
+  return result;
 }
 
 int check_luhn(long card_number) {
@@ -39,64 +49,33 @@ int check_luhn(long card_number) {
     3. The last 2 digits of the integer should represent first 2 digits of
     the card
     */
-
-    int first_two_digits;
+    long card = card_number;
     int count_digits = 0;
 
     // Sum of multiplied and not multiplied digits
     int sum_multiply = 0;
     int sum_not_multiply = 0;
 
-    // Shrink card number
-    while (card_number > 1000) {
-        int last_digit = card_number % 10;
-        sum_not_multiply += last_digit;
-        card_number /= 10;
-        count_digits++;
-
-        int second_to_last = card_number % 10;
-        // Check if product of the multiplied digit is more than 9. If so,
-        // sum its digits.
-        sum_multiply += is_double_digit(second_to_last);
-
-        card_number /= 10;
+    while (card > 0) {
+        int digit = card % 10;
+        if (count_digits % 2 == 0) {
+            sum_not_multiply += digit;
+        } else {
+            sum_multiply += is_double_digit(digit);
+        }
+        card /= 10;
         count_digits++;
     }
-
-    // Process the remainder
-    if (card_number < 100) {
-        sum_not_multiply += card_number % 10;
-
-        // Check if product of the multiplied digit is more than 9. If so,
-        // sum its digits.
-        sum_multiply += is_double_digit(card_number / 10);
-        // Add 2 to count_digits
-        count_digits += 2;
-
-        // Add first digits
-        first_two_digits = card_number;
-
-    } else {
-        sum_not_multiply += card_number % 10;
-        card_number /= 10;
-
-        // Check if product of the multiplied digit is more than 9. If so,
-        // sum its digits.
-        sum_multiply += is_double_digit(card_number % 10);
-        sum_not_multiply += card_number / 10;
-        // Add 3 to count_digits
-        count_digits += 3;
-
-        // Add first digits
-        first_two_digits = card_number;
-    }
-
-    // Initialize total variable to store sum of digits
     int total = sum_not_multiply + sum_multiply;
-    // Check conditions for checksum and for first digits
-    if (total % 10 != 0) {
+
+    // Return 0 if number didn't pass Luhn's algorithm or digit count less than
+    // 13 or  more than 16.
+    if (total % 10 != 0 || count_digits < 13 || count_digits > 16) {
         return 0;
     }
+
+    // Extract first 2 digits from the card number 
+    int first_two_digits = card_number / to_power(10, count_digits - 2);
     if (first_two_digits < 34 || first_two_digits > 55) {
         return 0;
     }
@@ -105,11 +84,14 @@ int check_luhn(long card_number) {
     return (count_digits + first_two_digits);
 }
 
+// Function that prints "INVALID" and returns 1
 int print_invalid(void) {
-    printf("print_INVALID");
+    printf("INVALID\n");
     return 1;
 }
 
+// Print the brand of the card and return 0, otherwise, print 'INVALID' 
+// and return 1.
 int print_result(int luhn_result) {
 
     if (luhn_result == 0) {
@@ -120,27 +102,27 @@ int print_result(int luhn_result) {
     luhn_result /= 100;
     int n_digits = luhn_result;
 
-    if (n_digits != 13 || n_digits != 15 || n_digits != 16) {
+    if (n_digits != 13 && n_digits != 15 && n_digits != 16) {
         return print_invalid();
     }
 
     // Visa card check
     if ((first_digits > 39 || first_digits < 50) &&
         (n_digits == 16 || n_digits == 13)) {
-        printf("VISA");
+        printf("VISA\n");
         return 0;
     }
 
     // Master card check
     if ((first_digits > 50 || first_digits < 56) && n_digits == 16) {
-        printf("MASTERCARD");
+        printf("MASTERCARD\n");
         return 0;
     }
 
     // Amex card check
     if ((first_digits == 34 || first_digits == 37) && n_digits == 15) {
-        printf("AMEX");
+        printf("AMEX\n");
         return 0;
     }
-
+    return print_invalid();
 }
