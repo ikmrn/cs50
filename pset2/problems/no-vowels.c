0e34d1f5663bb48c343cb59184848a96e14b5068
ikmrn <ibragimovkmrn@gmail.com>
2023-12-26 21:16:22 +0500

    feat: Add function to change vowels to digits

--- pset2/problems/no-vowels.c
+++ pset2/problems/no-vowels.c
@@ -1,12 +1,47 @@
-// Write a function to replace vowels with numbers
-// Get practice with strings
-// Get practice with command line
-// Get practice with switch
-
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, string argv[])
-{
-    
+// Function to replace vowels with numbers
+void no_vowels(string word);
+
+int main(int argc, string argv[]) {
+    // Check if the correct number of command-line arguments is provided
+    if (argc != 2) {
+        printf("Usage: ./no-vowels word\n");
+        return 1;
+    }
+
+    string word = argv[1];
+
+    // Call the function to replace vowels with numbers
+    no_vowels(word);
+
+    printf("%s\n", word);
+}
+
+void no_vowels(string word) {
+    int str_length = strlen(word);
+    // Loop through each character in the word
+    for (int i = 0; i < str_length; i++) {
+        // Use a switch statement to replace vowels with numbers
+        switch (word[i]) {
+        case 'e':
+        case 'E':
+            word[i] = '3';
+            break;
+        case 'a':
+        case 'A':
+            word[i] = '6';
+            break;
+        case 'i':
+        case 'I':
+            word[i] = '1';
+            break;
+        case 'o':
+        case 'O':
+            word[i] = '0';
+            break;
+        }
+    }
 }
