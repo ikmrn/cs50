265ddbc1924df964b3ef7c8930a6f8a7c303acdf
ikmrn <ibragimovkmrn@gmail.com>
2024-01-10 21:58:13 +0500

    feat: Copy 'plates.txt' to array plates

--- pset4/problems/license/license.c
+++ pset4/problems/license/license.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     // Check for command line args
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("Usage: ./read infile\n");
         return 1;
     }
@@ -19,18 +19,18 @@ int main(int argc, char *argv[])
 
     int idx = 0;
 
-    while (fread(buffer, 1, 7, infile) == 7)
-    {
+    while (fread(&buffer, 1, 7, infile) == 7) {
         // Replace '\n' with '\0'
         buffer[6] = '\0';
-
+        printf("Buffer %i: %s\n", idx, buffer);
+        plates[idx] = malloc(sizeof(buffer));
         // Save plate number in array
-        plates[idx] = buffer;
+        strcpy(plates[idx], buffer);
+        free(plates[idx]);
         idx++;
     }
 
-    for (int i = 0; i < 8; i++)
-    {
+    for (int i = 0; i < 8; i++) {
         printf("%s\n", plates[i]);
     }
 }
