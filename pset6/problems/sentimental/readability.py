f13db78092a43a8d5ae49dffbb7f9ba1a1ed8682
ikmrn <ibragimovkmrn@gmail.com>
2024-01-17 20:11:44 +0500

    feat: Add 'count_text' function

--- pset6/problems/sentimental/readability.py
+++ pset6/problems/sentimental/readability.py
@@ -20,6 +20,50 @@ def main():
     print_grade(index)
 
 
+def count_text(text: str) -> tuple[int, int, int]:
+    """Count the number of letters, words, and sentences in the given text.
+
+    Parameters
+    ----------
+    text : str
+        The input text for counting.
+
+    Returns
+    -------
+    tuple[int, int, int]
+        A tuple containing counts of letters, words, and sentences.
+    """
+    i = 0
+    letters = 0
+    words = 0
+    sentences = 0
+    is_word = False
+
+    while i < len(text):
+        # Count letters, words and handle the beginning of a word
+        if text[i].isalpha():
+            letters += 1
+            i += 1
+            if not is_word:
+                words += 1
+                is_word = True
+        # Handle spaces to mark the end of a word
+        elif text[i].isspace():
+            i += 1
+            is_word = False
+        # Count sentences and mark the end of a word
+        elif text[i] == "?" or text[i] == "." or text[i] == "!":
+            sentences += 1
+            is_word = False
+            i += 1
+        # Skip non-alphabetic and non-space characters
+        else:
+            i += 1
+
+    return (letters, words, sentences)
+
+
+
 
 
 if __name__ == "__main__":
