008219bd122974e9814be10e59419eac38f5978f
ikmrn <ibragimovkmrn@gmail.com>
2024-03-21 12:57:18 +0500

    feat: Add bubble_sort function

--- pset3/lab/sort/bubble.c
+++ pset3/lab/sort/bubble.c
@@ -3,6 +3,7 @@
 
 // Prototypes
 int populate_array(int arr[], int capacity, FILE *file);
+void bubble_sort(int arr[], int len);
 
 int main(int argc, char *argv[]) {
     // Check the number of arguments
@@ -31,6 +32,7 @@ int main(int argc, char *argv[]) {
     }
     int len = populate_array(arr, capacity, file);
 
+    bubble_sort(arr, len - 1);
     for (int i = 0; i < len; i++) {
         printf("%i\n", arr[i]);
     }
@@ -50,4 +52,20 @@ int populate_array(int arr[], int capacity, FILE *file) {
     fclose(file);
 
     return count;
+}
+
+void bubble_sort(int arr[], int len) {
+    int permutation = 1;
+
+    for (int i = 0; i < len && permutation != 0; i++) {
+        permutation = 0;
+        for (int j = 0; j < len - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                permutation = 1;
+            }
+        }
+    }
 }
