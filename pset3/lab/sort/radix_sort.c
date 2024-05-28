e8c47c5bbdc3da60512f13cf61f04bead62d1085
ikmrn <ibragimovkmrn@gmail.com>
2024-05-28 22:06:43 +0500

    feat: Add radix sort

--- pset3/lab/sort/radix_sort.c
+++ pset3/lab/sort/radix_sort.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int populate_array(int arr[], int len, FILE *file);
+void radix_sort(int arr[], int len);
+void count_sort(int arr[], int len, int exp);
+int find_max(int arr[], int len);
 
 int main(int argc, char *argv[]) {
     // Check for command line arguments
@@ -29,6 +31,7 @@ int main(int argc, char *argv[]) {
     // Create array to read the integers
     int arr[capacity];
     int len = populate_array(arr, capacity, file);
+    radix_sort(arr, len);
 
     for (int i = 0; i < len; i++) {
         printf("%i\n", arr[i]);
@@ -49,4 +52,46 @@ int populate_array(int arr[], int len, FILE *file) {
     }
     fclose(file);
     return count;
+}
+
+// Function to find the maximum element in the array
+int find_max(int arr[], int len) {
+    int max = arr[0];
+    for (int i = 1; i < len; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void count_sort(int arr[], int len, int exp) {
+    int count[10] = {0};
+    int output[len + 1];
+
+    // Count the number of occurrences of each digit
+    for (int i = 0; i < len; i++) {
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    // Adjust the actual position of in the array
+    for (int i = 1; i < 10; i++) count[i] += count[i - 1];
+
+    // Place the elements in sorted order
+    for (int i = len - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+    for (int i = 0; i < len; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radix_sort(int arr[], int len) {
+    // Find the maximum element in the array
+    int max = find_max(arr, len);
+
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        count_sort(arr, len, exp);
+    }
 }
